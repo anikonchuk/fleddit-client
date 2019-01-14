7fb09c6cc721a0e1bb1f259a483b413528762ee2
anikonchuk <anikonch@gmail.com>
2019-01-14 15:50:29 -0500

    Update links in Navigation Bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-default navbar-gray">
       <div className="container-fluid">
         <div className="navbar-header">
-          <a href="#" class="navbar-brand">Fleddit</a>
+          <Link to='/'>Fleddit </Link>
         </div>
         <div className="mr-auto">
           <ul className="nav navbar-nav">
-            <li>All Posts</li>
-            <li>New Post</li>
-            <li>About</li>
+            <li><Link to='/posts'>All Posts</Link></li>
+            <li><Link to='/posts/new'>New Post</Link></li>
+            <li><Link to='/about'>About</Link></li>
           </ul>
         </div>
       </div>
