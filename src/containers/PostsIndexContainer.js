55cc9bdf84255f4f4b0165737f9e6cd05c2cf34c
anikonchuk <anikonch@gmail.com>
2019-01-14 16:21:30 -0500

    Set up skeleton PostsIndexContainer

---
+++ src/containers/PostsIndexContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class PostsIndexController extends Component {
+
+  render() {
+    return(
+      <div>
+        <h2>This is the PostsIndexController</h2>
+      </div>
+    )
+  }
+}
