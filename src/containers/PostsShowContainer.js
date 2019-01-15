62513a96ff0fb6ba14c4164308b369a4c9cbf55d
anikonchuk <anikonch@gmail.com>
2019-01-15 12:54:17 -0500

    Add PostsShowContainer skeleton

---
+++ src/containers/PostsShowContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShowContainer extends Component {
+  render() {
+    return(
+      <div>
+        <h2>This is the PostsShowContainer</h2>
+      </div>
+    )
+  }
+}
+
+export default PostsShowContainer
