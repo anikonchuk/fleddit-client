6623c9b4fae0b5bede7035c47a5586b28f103923
anikonchuk <anikonch@gmail.com>
2019-01-15 13:42:55 -0500

    Add PostCard component with skeleton data

---
+++ src/components/PostCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostCard = ({ targetPost }) => {
+  <div className="post-card">
+    <h2>{targetPost.title}</h2>
+    <h4>by: {targetPost.author}</h4>
+    { targetPost.img_url ? <img src={targetPost.img_url} alt={targetPost.title}/> : null }
+    <p>{targetPost.content}</p>
+  </div>
+}
+
+export default PostCard
