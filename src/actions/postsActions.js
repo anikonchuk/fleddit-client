b75af6e3916086dec247e24fbdcb739ec99007ab
anikonchuk <anikonch@gmail.com>
2019-01-15 13:25:32 -0500

    Add fetchTargetPost action to postActions

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -6,3 +6,12 @@ export function fetchAllPosts() {
       .then(posts => dispatch({type: 'ADD_ALL_POSTS', posts}));
   };
 }
+
+export function fetchTargetPost(postId) {
+  return(dispatch) => {
+    dispatch({type: 'START_ADDING_POSTS'});
+    return fetch(`http://localhost:3005/api/posts/${postId}`)
+      .then(response => response.json())
+      .then(post => dispatch({type: 'ADD_TARGET_POST', post}))
+  }
+}
