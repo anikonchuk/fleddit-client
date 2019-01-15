89e41ed2c1570f9045c8605abdf4675c2a90fb38
anikonchuk <anikonch@gmail.com>
2019-01-15 16:11:29 -0500

    Add fetchAllComments action to commentsActions.js

---
+++ src/actions/commentsActions.js
@@ -0,0 +1,8 @@
+export function fetchAllComments() {
+  return(dispatch) => {
+    dispatch({type: 'START_ADDING_COMMENTS'});
+    return fetch('http://localhost:3005/api/comments')
+      .then(response => response.json())
+      .then(comments => dispatch({type: 'ADD_ALL_POSTS', comments}));
+  };
+}
