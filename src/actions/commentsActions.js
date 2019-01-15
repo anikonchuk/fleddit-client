6dbb4e9a5ce2f840f90a93448b87899e0b405782
anikonchuk <anikonch@gmail.com>
2019-01-15 16:12:40 -0500

    Revise fetchAllComments

--- src/actions/commentsActions.js
+++ src/actions/commentsActions.js
@@ -3,6 +3,6 @@ export function fetchAllComments() {
     dispatch({type: 'START_ADDING_COMMENTS'});
     return fetch('http://localhost:3005/api/comments')
       .then(response => response.json())
-      .then(comments => dispatch({type: 'ADD_ALL_POSTS', comments}));
+      .then(comments => dispatch({type: 'ADD_ALL_COMMENTS', comments}));
   };
 }
