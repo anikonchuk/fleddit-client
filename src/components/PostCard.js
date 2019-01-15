02fe5d736243e9a3ef558cded893333ec5e947df
anikonchuk <anikonch@gmail.com>
2019-01-15 15:28:39 -0500

    Change layout on PostCard component

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -5,8 +5,8 @@ const PostCard = ({ targetPost }) => {
     <div className="post-card">
       <h2>{targetPost.title}</h2>
       <h4>by: {targetPost.author}</h4>
-      { targetPost.img_url ? <img src={targetPost.img_url} alt={targetPost.title}/> : null }
       <p>{targetPost.content}</p>
+      { targetPost.img_url ? <img src={targetPost.img_url} alt={targetPost.title}/> : null }
     </div>
   )
 }
