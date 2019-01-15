b63357a09cc44e88e20ce660cd8c911f9c335712
anikonchuk <anikonch@gmail.com>
2019-01-15 16:18:32 -0500

    Connect CommentsContainer to Redux store and add fetch action to componentDidMount

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchAllComments } from '../actions/commentsActions'
 
 class CommentsContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchAllComments();
+  }
+
   render() {
     return(
       <div className="comments-container">
         <h3><em>Comments</em></h3>
+        {this.props.loading ? <h3>Loading Comments</h3> : null}
       </div>
     )
   }
 }
 
-export default CommentsContainer
+const mapStateToProps = state => {
+  return {
+    comments: state.comments.comments,
+    loading: state.comments.loading
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAllComments: () => {
+      dispatch(fetchAllComments())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentsContainer)
