74e0e3c6aeaa4bd09f0a37b20e86e9225b4bd223
anikonchuk <anikonch@gmail.com>
2019-01-15 12:50:29 -0500

    Add link to post show page to PostPreview component

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PostPreview = (props) => {
   return (
     <div className="post-preview">
-      <h5>{props.post.title}</h5>
+      <h5><Link to={`/posts/${props.post.id}`}>{props.post.title}</Link></h5>
       <p>{props.post.author}</p>
     </div>
   )
