6baa9ac45f287280cd0828813009de58f1d28a3a
anikonchuk <anikonch@gmail.com>
2019-01-15 16:03:15 -0500

    Create CommentsContainer and add skeleton data

---
+++ src/containers/CommentsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentsContainer extends Component {
+  render() {
+    return(
+      <div className="comments-container">
+        <h3><em>Comments</em></h3>
+      </div>
+    )
+  }
+}
+
+export default CommentsContainer
