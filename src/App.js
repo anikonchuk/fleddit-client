b6abaafa27a315a78112847f29777a5fb4596ad9
anikonchuk <anikonch@gmail.com>
2019-01-15 13:11:08 -0500

    Add Route for Posts Show page to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavigationBar from './components/NavigationBar'
 import Welcome from './components/Welcome'
 import About from './components/About'
 import PostsIndexContainer from './containers/PostsIndexContainer'
+import PostsShowContainer from './containers/PostsShowContainer'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <Route exact path='/' component={Welcome}/>
           <Route exact path='/about' component={About}/>
           <Route exact path='/posts' component={PostsIndexContainer}/>
+          <Route path={`/posts/:postId`} component={PostsShowContainer}/>
         </Switch>
       </div>
     );
