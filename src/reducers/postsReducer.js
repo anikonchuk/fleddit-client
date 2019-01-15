5428f7d6fa8f9e400629e18d6b6b1a21f86865e2
anikonchuk <anikonch@gmail.com>
2019-01-15 11:24:08 -0500

    Add cases to PostsReducer for fetching all posts

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -5,6 +5,10 @@ export default function postsReducer (
   }, action
 ) {
   switch(action.type) {
+    case 'START_ADDING_ALL_POSTS':
+      return {...state, loading: true}
+    case 'ADD_ALL_POSTS':
+      return {loading:false, posts: action.payload}
     default:
       return state;
   }
