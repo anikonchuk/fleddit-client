b7a12a9d960ea7e1092aa6dfab2f8b5516aa90c2
anikonchuk <anikonch@gmail.com>
2019-01-16 10:25:26 -0500

    Add form wrapper to PostForm

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -19,15 +19,17 @@ class PostForm extends Component {
     return(
       <div className="post-form">
         <p><em>Title, author, and content are required.</em></p>
-        <label htmlFor="title">Title</label><br/>
-        <input type="text" id="title" name="title" value={this.state.title} onChange={this.handleChange} required /><br/>
-        <label htmlFor="author">Author</label><br/>
-        <input type="text" id="author" name="author" value={this.state.author} onChange={this.handleChange} required /><br/>
-        <label htmlFor="content">Content</label><br/>
-        <textarea id="content" name="content" value={this.state.content} onChange={this.handleChange} required ></textarea><br/>
-        <label htmlFor="img_url">Image URL</label><br/>
-        <input type="text" id="img_url" name="img_url" value={this.state.img_url} onChange={this.handleChange} /><br/>
-        <input type="submit" className="btn btn-primary"/>
+        <form>
+          <label htmlFor="title">Title</label><br/>
+          <input type="text" id="title" name="title" value={this.state.title} onChange={this.handleChange} required /><br/>
+          <label htmlFor="author">Author</label><br/>
+          <input type="text" id="author" name="author" value={this.state.author} onChange={this.handleChange} required /><br/>
+          <label htmlFor="content">Content</label><br/>
+          <textarea id="content" name="content" value={this.state.content} onChange={this.handleChange} required ></textarea><br/>
+          <label htmlFor="img_url">Image URL</label><br/>
+          <input type="text" id="img_url" name="img_url" value={this.state.img_url} onChange={this.handleChange} /><br/>
+          <input type="submit" className="btn btn-primary"/>
+        </form>
       </div>
     )
   }
