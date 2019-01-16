15993bcef3d9d2c829be8704233f9e3b19bb1bdb
anikonchuk <anikonch@gmail.com>
2019-01-16 09:44:11 -0500

    Add styling to .post-form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class PostForm extends Component {
   render() {
     return(
-      <div>
-      This is the post form.
+      <div className="post-form">
+      <p><em>Title, author, and content are required.</em></p>
       </div>
     )
   }
