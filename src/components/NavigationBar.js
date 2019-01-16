7ddeb14e49c8cce5366857b64c13829b959bdc4c
anikonchuk <anikonch@gmail.com>
2019-01-16 14:37:07 -0500

    Remove unneeded code in NavigationBar component

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -43,17 +43,3 @@ const NavigationBar = () => {
 }
 
 export default NavigationBar
-
-// <nav className="navbar navbar-default navbar-gray">
-//   <div className="container-fluid">
-//     <div className="navbar-header">
-//       <Link to='/'>Fleddit </Link>
-//     </div>
-//     <div>
-//       <ul className="nav navbar-nav">
-//         <li><Link to='/posts'>All Posts</Link></li>
-//         <li><Link to='/about'>About</Link></li>
-//       </ul>
-//     </div>
-//   </div>
-// </nav>
