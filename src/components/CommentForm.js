c1d571033e364dc18edbd375780a3bd9aac8949f
anikonchuk <anikonch@gmail.com>
2019-01-16 11:51:46 -0500

    Create CommentForm component skeleton

---
+++ src/components/CommentForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class CommentForm extends Component {
+  render() {
+    return(
+      <div>
+      </div>
+    )
+  }
+}
