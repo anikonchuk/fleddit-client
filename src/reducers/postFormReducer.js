95fda54f62e84680138f701da18ee2704cfcf820
anikonchuk <anikonch@gmail.com>
2019-01-16 11:03:57 -0500

    Build out postFormReducer

--- src/reducers/postFormReducer.js
+++ src/reducers/postFormReducer.js
@@ -7,6 +7,10 @@ const initialState = {
 
 export default function postFormReducer(state = initialState, action) {
   switch(action.type) {
+    case 'UPDATED_POST_DATA':
+      return action.postFormData;
+    case 'RESET_POST_FORM':
+      return initialState;
     default:
       return state;
   }
