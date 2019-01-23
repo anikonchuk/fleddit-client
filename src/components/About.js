0371a0fcb15af147a551b41c46fc56b6c8fae025
anikonchuk <anikonch@gmail.com>
2019-01-23 12:55:52 -0500

    Revise About component

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return(
     <div>
       <h1>About Fleddit</h1>
-      <p>Fleddit is an app I built for my final project for the Online Web Development Program at the Flatiron School.</p>
+      <p>Fleddit is an app I built for my final project for the Online Full Stack Web Development Program at the Flatiron School.</p>
       <p>The back end of the website is a Ruby on Rails API. The front end was built using React and Redux.</p>
     </div>
   )
