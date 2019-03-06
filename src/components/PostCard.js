8a698f2030f004c9f278c337935a41c9151b0e93
anikonchuk <anikonch@gmail.com>
2019-03-06 09:26:19 -0500

    Change PostCard component to class component

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PostCard = ({ targetPost }) => {
-  return(
-    <div className="post-card">
-      <h2>{targetPost.title}</h2>
-      <h5><em>{targetPost.author}</em></h5>
-      <p>{targetPost.content}</p>
-      { targetPost.img_url ? <img src={targetPost.img_url} alt={targetPost.title}/> : null }
-    </div>
-  )
+class PostCard extends Component {
+
+  render() {
+    return(
+      <div className="post-card">
+        <h2>{this.props.targetPost.title}</h2>
+        <h5><em>{this.props.targetPost.author}</em></h5>
+        <p>{this.props.targetPost.content}</p>
+        { this.props.targetPost.img_url ? <img src={this.props.targetPost.img_url} alt={this.props.targetPost.title}/> : null }
+      </div>
+    )
+  }
 }
 
 export default PostCard
