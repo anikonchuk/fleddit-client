6a21404a12c9ee8348739400f36f13a37bf0fc23
anikonchuk <anikonch@gmail.com>
2019-03-13 11:14:22 -0400

    Add sorting method for posts by date added

--- src/containers/PostsIndexContainer.js
+++ src/containers/PostsIndexContainer.js
@@ -11,7 +11,10 @@ class PostsIndexContainer extends Component {
   }
 
   displayPostPreviews = () => {
-    return this.props.posts.map(post => <PostPreview key={post.id} post={post}/>)
+    let newPostOrder = this.props.posts.sort(function(postA, postB) {
+      return new Date(postA.created_at).getTime() - new Date(postB.created_at).getTime()
+    })
+    return newPostOrder.map(post => <PostPreview key={post.id} post={post}/>)
   }
 
   render() {
