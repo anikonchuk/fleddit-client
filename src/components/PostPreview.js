883cc9640c96caa4450b9eb31fc847352d99b1f0
anikonchuk <anikonch@gmail.com>
2019-03-13 10:47:50 -0400

    Add likes to PostPreview component

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -6,6 +6,7 @@ const PostPreview = (props) => {
     <div className="post-preview">
       <h5><Link to={`/posts/${props.post.id}`}>{props.post.title}</Link></h5>
       <p>{props.post.author}</p>
+      <p><em>{props.post.likes} likes</em></p>
     </div>
   )
 }
