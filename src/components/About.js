9780948d4b120cfe3347a9fa3686499f096bc26a
anikonchuk <anikonch@gmail.com>
2019-03-13 11:01:48 -0400

    Add box shadow to About div

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,10 @@ const About = () => {
   return(
     <div>
       <h1>About Fleddit</h1>
-      <p>Fleddit is an app I built for my final project for the Online Full Stack Web Development Program at the Flatiron School.</p>
-      <p>The back end of the website is a Ruby on Rails API. The front end was built using React and Redux.</p>
+      <div className="about-card">
+        <p>Fleddit is an app I built for my final project for the Online Full Stack Web Development Program at the Flatiron School.</p>
+        <p>The back end of the website is a Ruby on Rails API. The front end was built using React and Redux.</p>
+      </div>
     </div>
   )
 }
